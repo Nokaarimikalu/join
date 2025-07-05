c20b812b4af65f53b8d020dda9161f2bb8a67958
Pascal Fliedner <fliednerpascal@gmail.com>
2025-07-05 19:17:22 +0200

    Overlay erstellt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OverlayContactsComponent } from './contacts/overlay-contacts/overlay-contacts.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, OverlayContactsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
