d067430efdd7724eb21788bd91b6bdd6b594cd85
Pascal Fliedner <fliednerpascal@gmail.com>
2025-07-05 17:51:38 +0200

    Contacts, Header und Nav/Footer Datein erstellt

---
+++ src/app/contacts/overlay-contacts/overlay-contacts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-overlay-contacts',
+  imports: [],
+  templateUrl: './overlay-contacts.component.html',
+  styleUrl: './overlay-contacts.component.scss'
+})
+export class OverlayContactsComponent {
+
+}
