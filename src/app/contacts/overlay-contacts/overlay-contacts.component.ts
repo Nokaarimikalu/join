d145f11df89c91f23500f5a6ddc190a9e65ced47
twenty2industries <burak.bostanli.1@web.de>
2025-07-06 20:45:50 +0200

    feat:
    overlay-contacts.components edited

--- src/app/contacts/overlay-contacts/overlay-contacts.component.ts
+++ src/app/contacts/overlay-contacts/overlay-contacts.component.ts
@@ -4,33 +4,35 @@ import { FormsModule } from '@angular/forms';
 import { ContactList } from '../../shared/interface/contact-list.interface';
 
 @Component({
-  selector: 'app-overlay-contacts',
-  imports: [FormsModule],
-  templateUrl: './overlay-contacts.component.html',
-  styleUrl: './overlay-contacts.component.scss',
+    selector: 'app-overlay-contacts',
+    imports: [FormsModule],
+    templateUrl: './overlay-contacts.component.html',
+    styleUrl: './overlay-contacts.component.scss',
 })
 export class OverlayContactsComponent {
-  newContact: ContactList = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    phone: '',
-    initials: '',
-  };
+    fullName: string = '';
 
-  constructor(public overlayState: OverlayState) { }
-
-  toggleOverlay() {
-    this.overlayState.toggleOverlay();
-  }
-
-  resetNewContactInput() {
-    this.newContact = {
-      firstName: '',
-      lastName: '',
-      email: '',
-      phone: '',
-      initials: '',
+    newContact: ContactList = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+        initials: '',
     };
-  }
+
+    constructor(public overlayState: OverlayState) {}
+
+    toggleOverlay() {
+        this.overlayState.toggleOverlay();
+    }
+
+    resetNewContactInput() {
+        this.newContact = {
+            firstName: '',
+            lastName: '',
+            email: '',
+            phone: '',
+            initials: '',
+        };
+    }
 }
