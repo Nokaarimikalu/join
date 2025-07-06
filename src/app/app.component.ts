100ffa7de272f373447ca6e210632d94a039e970
Maik-B <maikbennefeld@googlemail.com>
2025-07-06 10:08:55 +0200

    style-fix
    fixed asset path in html files, changed font-sizes for new font-family

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavFooterComponent } from './shared/nav-footer/nav-footer.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavFooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
