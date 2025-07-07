7ce6dd516ad905bd0c29b41cf3ba269fac2c5265
Irina Gorges <r3nategr4nate@gmail.com>
2025-07-07 12:43:06 +0200

    added mobile edit/delete button

--- src/app/contacts/info-screen/info-screen.component.ts
+++ src/app/contacts/info-screen/info-screen.component.ts
@@ -1,11 +1,20 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-info-screen',
-  imports: [],
-  templateUrl: './info-screen.component.html',
-  styleUrl: './info-screen.component.scss'
+    selector: 'app-info-screen',
+    imports: [NgIf],
+    templateUrl: './info-screen.component.html',
+    styleUrl: './info-screen.component.scss',
 })
 export class InfoScreenComponent {
+    isDropdownOpen = false;
 
+    toggleDropdown(): void {
+        this.isDropdownOpen = !this.isDropdownOpen;
+    }
+
+    closeDropdown(): void {
+        this.isDropdownOpen = false;
+    }
 }
