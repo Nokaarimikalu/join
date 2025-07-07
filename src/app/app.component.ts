fbecdd97632e6e872248de0512616cc587173e81
twenty2industries <burak.bostanli.1@web.de>
2025-07-07 11:05:17 +0200

    Feat&Style:  merged all components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { OverlayContactsComponent } from './contacts/overlay-contacts/overlay-contacts.component';
-import { ContactListComponent } from "./contacts/contact-list/contact-list.component";
-import { NavFooterComponent } from './shared/nav-footer/nav-footer.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { InfoScreenComponent } from './contacts/info-screen/info-screen.component';
-
+import { ContactsComponent } from './contacts/contacts.component';
+ContactsComponent
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavFooterComponent, HeaderComponent, OverlayContactsComponent, ContactListComponent, InfoScreenComponent],
+  imports: [RouterOutlet, ContactsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
