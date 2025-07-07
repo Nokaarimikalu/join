b4979b846086220ec2ad55cc568e10619e0d0ad1
Irina Gorges <r3nategr4nate@gmail.com>
2025-07-07 09:40:07 +0200

    style: contactinfo.component done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InfoScreenComponent } from './contacts/info-screen/info-screen.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InfoScreenComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Join';
