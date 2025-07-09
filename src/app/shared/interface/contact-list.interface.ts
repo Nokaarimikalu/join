adc58f578abbccd3b3bb6aef6c2176f84052b83d
Maik-B <maikbennefeld@googlemail.com>
2025-07-09 10:20:49 +0200

    full clear to reset branch
    reset branch cause of commit and merge issues.

--- src/app/shared/interface/contact-list.interface.ts
+++
@@ -1,8 +0,0 @@
-    export interface ContactList {
-      id?: string;
-      firstName: string;
-      lastName: string;
-      email: string;
-      phone: string;
-      initials?: string;
-    }
