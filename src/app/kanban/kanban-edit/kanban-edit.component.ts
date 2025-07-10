bfc3ece232c98933246b2c8ad9e663f0f67e404d
twenty2industries <burak.bostanli.1@web.de>
2025-07-10 19:19:10 +0200

    sprint 2 file structure created

---
+++ src/app/kanban/kanban-edit/kanban-edit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kanban-edit',
+  imports: [],
+  templateUrl: './kanban-edit.component.html',
+  styleUrl: './kanban-edit.component.scss'
+})
+export class KanbanEditComponent {
+
+}
