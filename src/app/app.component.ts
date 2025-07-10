12463e27224094482b3a83c8827310f6fc02a717
Maik-B <maikbennefeld@googlemail.com>
2025-07-10 18:03:35 +0200

    branch restore from backup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContactsComponent } from './contacts/contacts.component';
 
-
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, ContactsComponent],
