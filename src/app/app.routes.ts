1ef54d12126e662be11acb8fbcb728d97c0cc070
twenty2industries <burak.bostanli.1@web.de>
2025-07-11 10:34:59 +0200

    Refactor routing and update Kanban and nav components
    Updated app routes to use KanbanComponent as the default and for '/board', and changed '/contacts' to use ContactsComponent. Refactored KanbanComponent to include header and navigation components, added corresponding SCSS for layout, and improved mobile nav footer to use router links for navigation. Also ensured consistent styling for navigation links.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { ContactsComponent } from "../app/contacts/contacts.component";
 import { KanbanEditComponent } from './kanban/kanban-edit/kanban-edit.component';
+import { KanbanComponent } from "./kanban/kanban.component";
+
 export const routes: Routes = [
+    {path: '', component:KanbanComponent},
+  {path: 'board', component:KanbanComponent},
   {path: 'edit', component:KanbanEditComponent},
-  {path: '', component: ContactsComponent },
+  {path: 'contacts', component: ContactsComponent },
 ];
--- src/app/kanban/kanban.component.ts
+++ src/app/kanban/kanban.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../shared/header/header.component';
+import { NavFooterComponent } from '../shared/nav-footer/nav-footer.component';
+import { NavFooterMobileComponent } from '../shared/nav-footer-mobile/nav-footer-mobile.component';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-kanban',
-  imports: [],
+  imports: [RouterLink, HeaderComponent,NavFooterComponent,NavFooterMobileComponent],
   templateUrl: './kanban.component.html',
   styleUrl: './kanban.component.scss'
 })
