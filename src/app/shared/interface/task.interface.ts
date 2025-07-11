9d072e35b94aa1d6246693bf0a0507ba6d7d6025
twenty2industries <burak.bostanli.1@web.de>
2025-07-11 19:39:58 +0200

    Add TaskItem interface definition
    Introduces the TaskItem interface to define the structure of a task, including fields for description, due date, priority, assignment, and subtasks.

---
+++ src/app/shared/interface/task.interface.ts
@@ -0,0 +1,11 @@
+export interface TaskItem {
+  id?: string;
+  title?: string;
+  description: string;
+  dueDate: Date;
+  priority: number | string;
+  assignedTo?: any;
+  subTask: {
+    task: string;
+  }[];
+}
