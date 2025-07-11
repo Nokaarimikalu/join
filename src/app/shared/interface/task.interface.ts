fb8eaada9f6bded259da0941a35383943bdea02e
Pascal Fliedner <fliednerpascal@gmail.com>
2025-07-12 01:17:51 +0200

    Refactor kanban edit UI and task model
    Updated the kanban edit overlay to support dynamic task editing, improved priority button UI with active states and new SVG icons, and enhanced subtask input interactions. Refactored the TaskItem interface to simplify subtask and assignedTo structures, and adjusted dummyTasks data and component logic accordingly.

--- src/app/shared/interface/task.interface.ts
+++ src/app/shared/interface/task.interface.ts
@@ -1,11 +1,12 @@
 export interface TaskItem {
   id?: string;
+  category?: string;
   title?: string;
   description: string;
-  dueDate: Date;
+  dueDate: string;
   priority: number | string;
-  assignedTo?: any;
-  subTask: {
-    task: string;
+  assignedTo?: {
+    user: string;
   }[];
+  subTask:string[];
 }
