f407331a122d641a79cbf07986d07455902f8835
twenty2industries <burak.bostanli.1@web.de>
2025-07-11 21:28:52 +0200

    Replace test containers with CardComponent in Kanban board
    Replaced placeholder divs with <app-card> components in the Kanban board columns. Moved the testcontainer styles from kanban-board.component.scss to card.component.scss and updated the CardComponent template to use the styled container. Also imported CardComponent into KanbanBoardComponent.

--- src/app/kanban/kanban-board/kanban-board.component.ts
+++ src/app/kanban/kanban-board/kanban-board.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { CardComponent } from './card/card.component';
 
 @Component({
   selector: 'app-kanban-board',
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './kanban-board.component.html',
   styleUrl: './kanban-board.component.scss'
 })
 export class KanbanBoardComponent {
 
 }
+
+
