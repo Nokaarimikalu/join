7a4dee4d17ab446f067a61d70f0caf53150509e1
Maik-B <maikbennefeld@googlemail.com>
2025-07-11 10:09:46 +0200

    added router links
    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,5 @@ import { ContactsComponent } from "../app/contacts/contacts.component";
 import { KanbanEditComponent } from './kanban/kanban-edit/kanban-edit.component';
 export const routes: Routes = [
   {path: 'edit', component:KanbanEditComponent},
-  { path: '', component: ContactsComponent },
+  {path: '', component: ContactsComponent },
 ];
