a1767c7d698f4a2a9667f83b9d93e8b15e19ae4b
Maik-B <maikbennefeld@googlemail.com>
2025-07-15 18:18:10 +0200

    Enable task creation via form binding
    Added two-way binding for task fields in the add task form and integrated form data with the BoardService. Updated TaskItemBoard interface to require title, and removed commented dummy tasks from board.service.ts for cleanup.

--- src/app/kanban/kanban-add/kanban-add.component.ts
+++ src/app/kanban/kanban-add/kanban-add.component.ts
@@ -3,13 +3,40 @@ import { RouterLink } from '@angular/router';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { NavFooterComponent } from '../../shared/nav-footer/nav-footer.component';
 import { NavFooterMobileComponent } from '../../shared/nav-footer-mobile/nav-footer-mobile.component';
+import { FormsModule } from '@angular/forms';
+import { BoardService } from '../../services/board/board.service';
+import { TaskItemBoard } from '../../shared/interface/task.interface';
 
 @Component({
   selector: 'app-kanban-add',
-  imports: [RouterLink, HeaderComponent, NavFooterComponent, NavFooterMobileComponent],
+  imports: [RouterLink, HeaderComponent, NavFooterComponent, NavFooterMobileComponent, FormsModule],
   templateUrl: './kanban-add.component.html',
   styleUrl: './kanban-add.component.scss'
 })
 export class KanbanAddComponent {
+
+  taskList: TaskItemBoard = {
+    id: '',
+    status: '',
+    title: '',
+    description: '',
+    dueDate: '',
+    priority: '',
+    assignedTo: [],
+    subTaskFillTest: []
+  };
+
+  constructor(public boardService: BoardService) {}
+
+  addTask() {
+    if (this.boardService.selectedTask) {
+      this.boardService.addTasks(this.boardService.selectedTask);
+      this.boardService.selectedTask = null;
+    }
+  }
+
+  toggleFullCard() {
+    this.boardService.fullCardActive = !this.boardService.fullCardActive;
+  }
 
 }
