3b4797887478b817c62675ad5d0497a5764d1a64
Maik-B <maikbennefeld@googlemail.com>
2025-07-18 08:27:09 +0200

    try catch case for undifined error

--- src/app/kanban/kanban-add/kanban-add.component.ts
+++ src/app/kanban/kanban-add/kanban-add.component.ts
@@ -120,8 +120,15 @@ export class KanbanAddComponent {
     }
 
     startEditingSubtask(index: number) {
-        this.editingSubtaskIndex = index;
-        this.editingSubtaskValue = this.task.subTaskFillTest[index].text;
+        try {
+            this.editingSubtaskIndex = index;
+            this.editingSubtaskValue = this.task.subTaskFillTest[index].text;
+        } catch (error) {
+            console.log('Editing subtask at index:', index);
+            console.log('Current subtask value:', this.taskList.subTaskFillTest[index].text);
+            console.log('Current taskList:', this.taskList.subTaskFillTest);
+        }
+        
     }
 
     saveEditingSubtask(index: number) {
