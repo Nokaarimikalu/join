1b5f32861577e0ebf297a83548eeab6c3e3488ca
twenty2industries <burak.bostanli.1@web.de>
2025-07-20 10:32:18 +0200

    RouterLink: Summary & signup added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ContactsComponent } from './contacts/contacts.component';
+import { LoginComponent } from './login/login.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ContactsComponent],
+  imports: [RouterOutlet, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,11 +7,15 @@ import { HelpMeComponent } from './shared/help-me/help-me.component';
 import { PrivacyComponent } from './shared/privacy/privacy.component';
 import { LegalNoticeComponent } from './shared/legal-notice/legal-notice.component';
 import { SummaryComponent } from './summary/summary.component';
+import { LoginComponent } from './login/login.component';
+import { SignupOverlayComponent } from './signup-overlay/signup-overlay.component';
 
 
 
 export const routes: Routes = [
-  {path: '', component:KanbanComponent},
+  {path: '', component:SummaryComponent},
+  {path: 'login', component:LoginComponent},
+  {path: 'signup', component:SignupOverlayComponent},
   {path: 'summary', component:SummaryComponent},
   {path: 'privacy', component:PrivacyComponent},
   {path: 'help', component:HelpMeComponent},
