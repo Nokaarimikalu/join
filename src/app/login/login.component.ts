52b4133e739caaa04e03aad7cd01b0cfa9d1474e
twenty2industries <burak.bostanli.1@web.de>
2025-07-22 21:11:45 +0200

    Feat: form Validation login done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,38 +1,41 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators, NgModel, NgForm, FormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../services/auth/auth.service';
 
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink, ReactiveFormsModule],
+  imports: [RouterLink, ReactiveFormsModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   fb = inject(FormBuilder);
   form = this.fb.nonNullable.group({
-    email: ['', Validators.required, Validators.email],
+    email: ['', [Validators.required, Validators.email]],
     password: ['', Validators.required],
   });
   authService = inject(AuthService);
 
-  
   errorMessage: string | null = null;
 
-  onSubmit(): void {
-    const rawForm = this.form.getRawValue();
-    this.authService.login(rawForm.email, rawForm.password)
-      .subscribe({
-        next: () => {
-        this.router.navigateByUrl('/');
-        },
-        error: (error) => {
-          this.errorMessage = error.code
-        },
-      });
+  constructor(private router: Router) { }
+
+onSubmit(): void {
+    this.form.markAllAsTouched();
+
+  if (this.form.invalid) {
+    this.errorMessage = 'Check your email and password. Please try again.';
+    return;
   }
+
+  const rawForm = this.form.getRawValue();
+  this.authService.login(rawForm.email, rawForm.password).subscribe({
+    next: () => this.router.navigateByUrl('/'),
+    error: (error) => this.errorMessage = error.code
+  });
+}
 
   guestLogIn(){
     this.authService.login('mustermann@bspmail.com', '123456')
@@ -46,7 +49,8 @@ export class LoginComponent {
       });
   }
 
-  constructor(private router: Router) { }
+
+
 }
 
 
