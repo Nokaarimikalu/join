909c2823db601ff769a0460e61b0d047e6201656
Irina Gorges <r3nategr4nate@gmail.com>
2025-07-23 12:11:14 +0200

    added show passwort function

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,55 +1,83 @@
 import { Component, inject, signal } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators, NgModel, NgForm, FormsModule } from '@angular/forms';
+import {
+    FormBuilder,
+    ReactiveFormsModule,
+    Validators,
+    NgModel,
+    NgForm,
+    FormsModule,
+} from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../services/auth/auth.service';
 
-
 @Component({
-  selector: 'app-login',
-  imports: [RouterLink, ReactiveFormsModule, FormsModule],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+    selector: 'app-login',
+    imports: [RouterLink, ReactiveFormsModule, FormsModule],
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  fb = inject(FormBuilder);
-  form = this.fb.nonNullable.group({
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', Validators.required],
-  });
-  authService = inject(AuthService);
+    fb = inject(FormBuilder);
+    form = this.fb.nonNullable.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required],
+    });
+    authService = inject(AuthService);
 
-  errorMessage: string | null = null;
+    errorMessage: string | null = null;
 
-  showPassword: boolean = false;
+    showPassword: boolean = false;
+    isFocused: boolean = false;
+    lockClickCount = 0;
 
+    lockIconSrc = 'assets/img/login/lock.svg'; // Standard-Bild
 
-  constructor(private router: Router) { }
+    checkLockClick(passwordInput: HTMLInputElement): void {
+        this.lockClickCount++;
 
-  onSubmit(): void {
-    this.form.markAllAsTouched();
-    if (this.form.invalid) {
-      this.errorMessage = 'Check your email and password. Please try again.';
-      return;
+        if (this.lockClickCount === 1) {
+            this.lockIconSrc = 'assets/img/login/visibility_off.svg'; // Bild 2
+            passwordInput.focus();
+        } else if (this.lockClickCount >= 2) {
+            this.showPassword = true;
+            this.lockIconSrc = 'assets/img/login/visibility.svg'; // Bild 3
+        }
     }
 
-    const rawForm = this.form.getRawValue();
-    this.authService.login(rawForm.email, rawForm.password).subscribe({
-      next: () => this.router.navigateByUrl('/'),
-      error: (error) => this.errorMessage = error.code
-    });
-  }
+    checkPasswordInput(): void {
+        const pwValue = this.form.get('password')?.value;
 
-  guestLogIn() {
-    this.authService.login('mustermann@bspmail.com', '123456')
-      .subscribe({
-        next: () => {
-          this.router.navigateByUrl('/');
+        if (this.lockClickCount < 1 && pwValue) {
+            this.lockIconSrc = 'assets/img/login/visibility_off.svg'; // Bild 2 beim erster Eingabe
         }
-      });
-  }
 
+        if (this.lockClickCount >= 2) {
+            this.lockIconSrc = 'assets/img/login/visibility.svg'; // Bild 3 bei weiterer Eingabe
+        }
+    }
 
+    constructor(private router: Router) {}
 
+    onSubmit(): void {
+        this.form.markAllAsTouched();
+        if (this.form.invalid) {
+            this.errorMessage =
+                'Check your email and password. Please try again.';
+            return;
+        }
+
+        const rawForm = this.form.getRawValue();
+        this.authService.login(rawForm.email, rawForm.password).subscribe({
+            next: () => this.router.navigateByUrl('/'),
+            error: (error) => (this.errorMessage = error.code),
+        });
+    }
+
+    guestLogIn() {
+        this.authService.login('mustermann@bspmail.com', '123456').subscribe({
+            next: () => {
+                this.router.navigateByUrl('/');
+            },
+        });
+    }
 }
-
-
